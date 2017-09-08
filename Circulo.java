7b9bba0201d8b39d12b455a776695e0598acd1e2
José Luis <jlfmtz_96@alumnos.uaslp.edu.mx>
2017-09-08 10:59:51 -0500

    Se agrega circulo sobre escribiendo el metodo calcularArea().

---
+++ Circulo.java
@@ -0,0 +1,20 @@
+/**
+ * Esta clase representa objetos circulo con un radio.
+ * @author JLFMTZ
+ * @version 1.0  (8-sep-17)
+ */
+public class Circulo extends Figura
+{
+    private float radio;
+    
+    public Circulo(float unRadio)
+    {
+        radio = unRadio;
+    }
+    
+    @Override
+    public void calcularArea()
+    {
+        area = 3.14f * radio * radio;
+    }
+}
